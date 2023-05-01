363961a960ab598e9d11bdc4295d76ebb692c4fc
anitabgp <anitabgp@gmail.com>
2023-05-01 16:51:19 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MaterialModule } from './material/material.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { BackgroundComponent } from './background/background.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    BackgroundComponent
   ],
   imports: [
     BrowserModule,
