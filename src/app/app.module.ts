0e2288943976019b8f07bdc32b54e3c515f915e2
anitabgp <anitabgp@gmail.com>
2023-05-02 17:34:16 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { BackgroundComponent } from './background/background.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RecepieComponent } from './recepie/recepie.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    BackgroundComponent
+    BackgroundComponent,
+    RecepieComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recepie/recepie.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recepie',
+  templateUrl: './recepie.component.html',
+  styleUrls: ['./recepie.component.scss']
+})
+export class RecepieComponent {
+
+}
