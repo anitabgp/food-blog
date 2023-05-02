a5adb620efdaa6a354991b73a12803708dcc019e
rohitashwabgp rohitashwabgp <rohitashwabgp@gmail.com>
2023-05-02 11:02:22 +0530

    removed unused

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MenuComponent } from '../menu/menu.component';
-import { BackgroundComponent } from '../background/background.component';
+import { MenuComponent } from './menu/menu.component';
+import { BackgroundComponent } from './background/background.component';
 
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { MaterialModule } from './material/material.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { BackgroundComponent } from './background/background.component';
-import { AppRoutingModule } from './app/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
