fdca228194aaad37839bd5322c0d1ebfa16a7214
anitabgp <anitabgp@gmail.com>
2023-05-03 22:37:30 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { BackgroundComponent } from './background/background.component';
+import { FoodComponent } from './food/food.component';
+import { RecepieComponent } from './recepie/recepie.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+import { RecepielatestComponent } from './recepielatest/recepielatest.component';
+import { TrendingRecepieComponent } from './trending-recepie/trending-recepie.component';
+import { FooterimgComponent } from './footerimg/footerimg.component';
+import { DocumentComponent } from './document/document.component';
+
 
 
 const routes: Routes = [
   { path: 'menu.component', component: MenuComponent },
   { path: 'background.component', component: BackgroundComponent},
+  { path: 'Food.component', component: FoodComponent },
+  { path: 'recepie.component', component: RecepieComponent },
+  { path: 'Subscribe.component', component: SubscribeComponent },
+  { path: 'Recepielatest.component', component: RecepielatestComponent },
+  { path: 'trendingrecepie.component', component: TrendingRecepieComponent },
+  { path: 'footerimg.component', component: FooterimgComponent },
+  { path: 'document.component', component: DocumentComponent },
 ];
 
 @NgModule({
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
+  constructor(private router: Router) { }
 
+  onClick(link:string) {
+  this.router.navigate(['/background.html']);
+  }
 }
