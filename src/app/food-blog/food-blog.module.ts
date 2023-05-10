b4345e036fac0c9b763cb0cf8aa0776c0e11b19e
anitabgp <anitabgp@gmail.com>
2023-05-10 11:44:53 +0530

    required

---
+++ src/app/food-blog/food-blog.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FoodBlogRoutingModule } from './food-blog-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FoodBlogRoutingModule
+  ]
+})
+export class FoodBlogModule { }
